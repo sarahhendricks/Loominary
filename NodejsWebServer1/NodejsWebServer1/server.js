2609c74f002a6c0646a3026dde6fb175caaa8692
Sarah Hendricks <slynnhendricks@gmail.com>
2016-11-21 15:13:06 -0500

    Initial commit
    3 base programs, server is not working

---
+++ NodejsWebServer1/NodejsWebServer1/server.js
@@ -0,0 +1,55 @@
+// Cannot see this from laptop!
+var http = require('http'),
+    fs = require('fs');
+var port = process.env.port || 1337;
+//fs.readFile('./bin/Loominosity.html', function (err, html) {
+//    if (err)
+//        console.log(err);
+//    http.createServer(function (req, res) {
+//        res.writeHead(200, { 'Content-Type': 'text/html' });
+//        res.write(html);
+//        res.end();
+//    }).listen(port);
+//});
+
+//var fileName = "Loominosity.html";
+//fs.exists(fileName, function (exists) {
+//    if (exists) {
+//        fs.stat(fileName, function (error, stats) {
+//            fs.open(fileName, "r", function (error, fd) {
+//                var buffer = new Buffer(stats.size);
+
+//                fs.read(fd, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+//                    var data = buffer.toString("utf8", 0, buffer.length);
+
+//                    console.log(data);
+//                    fs.close(fd);
+//                });
+//            });
+//        });
+//    }
+//    else {
+//        console.log("Does not exist");
+//    }
+//});
+
+
+var fileName = "Loominosity.html";
+fs.exists(fileName, function (exists) {
+    if (exists) {
+        fs.stat(fileName, function (error, stats) {
+            fs.readFile('Loominosity.html', function (err, html) {
+                if (err)
+                    console.log(err);
+                http.createServer(function (req, res) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(html);
+                    res.end();
+                }).listen(port);
+            }); 
+        });
+    }
+    else {
+        console.log("Does not exist");
+    }
+});
