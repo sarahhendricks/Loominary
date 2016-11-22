b89f8142b065454f8d57a2bf67cb0c9b35ee7940
Sarah Hendricks <slynnhendricks@gmail.com>
2016-11-21 20:58:12 -0500

    Adding in JSON test code & require Loominosity.html

--- NodejsWebServer1/NodejsWebServer1/bin/ARM/Debug/AppX/server.js
+++ NodejsWebServer1/NodejsWebServer1/bin/ARM/Debug/AppX/server.js
@@ -1,7 +1,9 @@
-// Cannot see this from laptop! It is a background app and runs on the pi.
+// Cannot see this from laptop!
 var http = require('http'),
     fs = require('fs');
 var port = process.env.port || 1337;
+var url = require("url");
+var queryString = require("querystring");
 //fs.readFile('./bin/Loominosity.html', function (err, html) {
 //    if (err)
 //        console.log(err);
@@ -33,6 +35,7 @@ var port = process.env.port || 1337;
 //    }
 //});
 
+
 var fileName = "Loominosity.html";
 fs.exists(fileName, function (exists) {
     if (exists) {
@@ -41,6 +44,22 @@ fs.exists(fileName, function (exists) {
                 if (err)
                     console.log(err);
                 http.createServer(function (req, res) {
+
+                    // Parsing JSON requests
+                    //// parses the request url
+                    //var theUrl = url.parse(req.url);
+
+                    //// gets the query part of the URL and parses it creating an object
+                    //var queryObj = queryString.parse(theUrl.query);
+
+                    //// queryObj will contain the data of the query as an object
+                    //// and jsonData will be a property of it
+                    //// so, using JSON.parse will parse the jsonData to create an object
+                    //var obj = JSON.parse(queryObj.jsonData);
+
+                    //// as the object is created, the live below will print "bar"
+                    //console.log(obj.foo);
+
                     res.writeHead(200, { 'Content-Type': 'text/html' });
                     res.write(html);
                     res.end();
--- NodejsWebServer1/NodejsWebServer1/server.js
+++ NodejsWebServer1/NodejsWebServer1/server.js
@@ -1,7 +1,9 @@
-// Cannot see this from laptop!
+// Cannot see this from laptop! Make sure to set Loominosity.html to "require in project"
 var http = require('http'),
     fs = require('fs');
 var port = process.env.port || 1337;
+var url = require("url");
+var queryString = require("querystring");
 //fs.readFile('./bin/Loominosity.html', function (err, html) {
 //    if (err)
 //        console.log(err);
@@ -42,6 +44,48 @@ fs.exists(fileName, function (exists) {
                 if (err)
                     console.log(err);
                 http.createServer(function (req, res) {
+
+                    // Parsing JSON requests
+                    //// parses the request url
+                    //var theUrl = url.parse(req.url);
+
+                    //// gets the query part of the URL and parses it creating an object
+                    //var queryObj = queryString.parse(theUrl.query);
+
+                    //// queryObj will contain the data of the query as an object
+                    //// and jsonData will be a property of it
+                    //// so, using JSON.parse will parse the jsonData to create an object
+                    //var obj = JSON.parse(queryObj.jsonData);
+
+                    //// as the object is created, the live below will print "bar"
+                    //console.log(obj.foo);
+
+
+                    //// Reading values from HTML form GET request
+                    //console.dir(req.param);
+
+                    //if (req.method == 'POST') {
+                    //    console.log("POST");
+                    //    var body = '';
+                    //    req.on('data', function (data) {
+                    //        body += data;
+                    //        console.log("Partial body: " + body);
+                    //    });
+                    //    req.on('end', function () {
+                    //        console.log("Body: " + body);
+                    //    });
+                    //    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    //    res.end('post received');
+                    //}
+                    //else {
+                    //    console.log("GET");
+                    //    //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
+                    //    var html = fs.readFileSync('index.html');
+                    //    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    //    res.end(html);
+                    //}
+
+
                     res.writeHead(200, { 'Content-Type': 'text/html' });
                     res.write(html);
                     res.end();
@@ -53,3 +97,39 @@ fs.exists(fileName, function (exists) {
         console.log("Does not exist");
     }
 });
+
+//<html>
+//    <body>
+//        <form method="post" action="http://localhost:3000">
+//            Name: <input type="text" name="name" />
+//            <input type="submit" value="Submit" />
+//        </form>
+
+//        <script type="text/JavaScript">
+//            console.log('begin');
+//            var http = new XMLHttpRequest();
+//            var params = "text=stuff";
+//            http.open("POST", "http://localhost:3000", true);
+
+//            http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//            //http.setRequestHeader("Content-length", params.length);
+//            //http.setRequestHeader("Connection", "close");
+
+//            http.onreadystatechange = function() {
+//                console.log('onreadystatechange');
+//            if (http.readyState == 4 && http.status == 200) {
+//                alert(http.responseText);
+//            }
+//            else {
+//                console.log('readyState=' + http.readyState + ', status: ' + http.status);
+//            }
+//            }
+
+//            console.log('sending...')
+//            http.send(params);
+//            console.log('end');
+
+//        </script>
+
+//    </body>
+//</html>
